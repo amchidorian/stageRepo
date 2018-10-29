dbac9b651385e2a06badb1c81b9f49f3cde74fe3
Unknown <amchi.dorian@outlook.fr>
2018-10-29 17:59:42 +0100

    MAJ Image / Bug add shortName to Array

--- testProjectBeforeInte.spritebuilder/SliderCryptoScene.swift
+++ testProjectBeforeInte.spritebuilder/SliderCryptoScene.swift
@@ -29,39 +29,38 @@ class SliderCryptoScene: CCNode, CCScrollViewDelegate {
     
     
     func didLoadFromCCB(){
-        for var i in 0..<100{lDataCrypto.append([:])}
-        let children:[CCNode]! = _scrollViewCrypto!.children as! [CCNode]
+        for _ in 0..<100{lDataCrypto.append([:])}
+        organizeCryptoDatas()
+        let children:[CCNode]! = _scrollViewCrypto!.children as? [CCNode]
         let slider = children[0]
-        //        createSliderLine(slider)
-        organizedCryptoDatas()
-        print(lDataCrypto[0])
+        createSliderLine(slider)
         
     }
     
     func createSliderLine(_ pSlider:CCNode){
-        var item:CCNode = CCBReader.load("ItemCrypto")
+        let item:CCNode = CCBReader.load("ItemCrypto")
         let ih = item.contentSize.height
         pSlider.contentSize.height = ih * 100
-        var sliderH = pSlider.contentSize.height
-        for var i in 0..<lListeItems.count{
+        let sliderH = pSlider.contentSize.height
+        for var i in 0..<lDataCrypto.count{
             var item:CCNode = CCBReader.load("ItemCrypto")
-            let ih = item.contentSize.height
-            let iFloat = CGFloat(i+1)
+            var iFloat = CGFloat(i+1)
             item.position = CGPoint.init(x: 0, y: sliderH - ih * iFloat)
             var itemChildren:Array = item.children
             for obj in itemChildren {
                 editItem(obj, i)
-                
             }
             pSlider.addChild(item)
         }
     }
     
     func editItem(_ lElement:Any, _ lIndex:Int){
+        print(lDataCrypto![lIndex])
         if let sprite = lElement as? CCSprite {
             if sprite.name.elementsEqual("light") {
                 let img = sprite.children[0] as! CCSprite
-                img.spriteFrame = CCSpriteFrame.init(imageNamed: "coinMarketCap/\(lListeItems[lIndex])")
+                print(lDataCrypto![lIndex]["img"]!)
+                img.spriteFrame = CCSpriteFrame.init(imageNamed: "coinMarketCap/\(lDataCrypto![lIndex]["img"]!)")
             }
         }
         if let colorNode = lElement as? CCNodeColor {
@@ -69,16 +68,26 @@ class SliderCryptoScene: CCNode, CCScrollViewDelegate {
         }
         if let label = lElement as? CCLabelTTF {
             if label.name.elementsEqual("cryptoName") {
-                label.string = lListeNomItems[lIndex]
+                label.string = lDataCrypto![lIndex]["name"]! as? String
+            }
+            if label.name.elementsEqual("shortName") {
+                label.string = lDataCrypto![lIndex]["shortName"]! as? String
+            }
+            if label.name.elementsEqual("value") {
+                label.string = lDataCrypto![lIndex]["price"]! as? String
+            }
+            if label.name.elementsEqual("percent") {
+                label.string = lDataCrypto![lIndex]["change7"]! as? String
             }
         }
     }
     
-    func organizedCryptoDatas(){
+    func organizeCryptoDatas(){
         if let cryptoDatas = HttpRequest.sCryptoDataParsed["data"] as? NSDictionary{
-            for(key,value) in cryptoDatas {
+            for(_,value) in cryptoDatas {
+                self.lTemplateCrypto = [:]
                 if let cryptoData = value as? NSDictionary{
-                    organizedCryptoData(cryptoData)
+                    organizeCryptoData(cryptoData)
                     let index = ((lTemplateCrypto["rank"] as? Int)!) - 1
                     lTemplateCrypto["img"] = "\(lTemplateCrypto["name"]!).png"
                     lDataCrypto![index] = self.lTemplateCrypto!
@@ -87,39 +96,48 @@ class SliderCryptoScene: CCNode, CCScrollViewDelegate {
         }
     }
     
-    func organizedCryptoData(_ pCryptoData:NSDictionary){
-        self.lTemplateCrypto = [:]
+    func organizeCryptoData(_ pCryptoData:NSDictionary){
         for(key,value) in pCryptoData {
             let jsonKey = key as? String
+            print(jsonKey)
             switch jsonKey {
             case "rank":
                 self.lTemplateCrypto["rank"] = value
+                print(lTemplateCrypto)
                 break
             case "name":
                 self.lTemplateCrypto["name"] = value
+                print(lTemplateCrypto)
                 break
             case "symbol" :
-                self.lTemplateCrypto["shortName"] = value
+                self.lTemplateCrypto["symbol"] = value
+                print(lTemplateCrypto)
                 break
             case "total_supply" :
                 self.lTemplateCrypto["total"] = value
+                print(lTemplateCrypto)
                 break
             case "price" :
                 self.lTemplateCrypto["price"] = value
+                print(lTemplateCrypto)
                 break
             case "percent_change_7d" :
                 self.lTemplateCrypto["change7"] = value
+                print(lTemplateCrypto)
                 break
             case "percent_change_24h" :
                 self.lTemplateCrypto["change24"] = value
+                print(lTemplateCrypto)
                 break
             case "quotes" :
                 let lTempData = unwrapJson(value)
-                organizedCryptoData(lTempData)
+                organizeCryptoData(lTempData)
+                print(lTemplateCrypto)
                 break
             case "USD" :
                 let lTempData = unwrapJson(value)
-                organizedCryptoData(lTempData)
+                organizeCryptoData(lTempData)
+                print(lTemplateCrypto)
                 break
             default:
                 break
@@ -131,20 +149,5 @@ class SliderCryptoScene: CCNode, CCScrollViewDelegate {
         let lDataToReturn = data as? NSDictionary
         return lDataToReturn!
     }
-    //        var lTempItems:[String] = []
-    //        var lTempNomItems:[String] = []
-    //        for var i in 0..<lListeItems.count{
-    //            for var j in 0..<lListeItems.count{
-    //                let lItem:[String] = lListeItems[j].components(separatedBy: "+_+")
-    //                if(Int(lItem[0]) == i){
-    //                    let lNom:String = lItem[1].components(separatedBy: ".png")[0]
-    //                    lTempNomItems.append(lNom)
-    //                    lTempItems.append(lListeItems[j])
-    //                }
-    //            }
-    //        }
-    //        lListeItems = lTempItems
-    //        lListeNomItems = lTempNomItems
-    
     
 }
